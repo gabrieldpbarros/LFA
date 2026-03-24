8985e0190292de2cacecce3641240a1369106884
gabriel <gabrielpanovich@gmail.com>
2026-03-23 23:40:56 -0300

    estrutura basica codigo DFA

--- Projetos/Códigos/DFA_func_ext.c
+++ Projetos/Códigos/DFA_func_ext.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    
+int** recebe_DFA(char* buffer){
+    // receber a entrada no formato [estado inicial] [estados finais] [-1] [transições];
+    // uma ideia para montar a matriz é percorrer toda a string do buffer e encontrar o
+    // maior número, então usá-lo para definir o tamanho da matriz por alocação dinâmica.
+}
 
+char* recebe_string(char* buffer){
+
+}
+
+int main(){
+    char buffer[1024];
+    int **DFA = recebe_DFA(buffer);
+    char *string = recebe_string(buffer);
+    
     return 0;
 }
