787051cb10f43edfdc5501a03382576e8bd66e56
gabriel <gabrielpanovich@gmail.com>
2026-03-25 22:18:25 -0300

    minor fixes: novo metodo de leitura de arquivos

--- Projetos/Códigos/DFA_func_ext.c
+++ Projetos/Códigos/DFA_func_ext.c
@@ -10,7 +10,7 @@ int encontra_maior(char* string){
     int max_size = strlen(string);
 
     for (int i = 0; i < max_size; i++) {
-        if (string[i] == '-') i += 2; // pula o '-1'
+        if (string[i] == '-') i += 1; // pula o '-'
         if (string[i] - '0' < 0) break;
         if ((int) string[i] - '0' > maior) maior = (int) string[i] - '0';
     }
@@ -18,9 +18,9 @@ int encontra_maior(char* string){
     return maior + 1;
 }
 
-int** recebe_DFA(char* buffer, int* inicial, int** finais, int* qtd_estados, int* qtd_finais){
-    // recebe a entrada no formato [estado inicial] [estados finais] [-1] [transições];
-    if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) return NULL;
+int** recebe_DFA(char* buffer, int* inicial, int** finais, int* qtd_estados, int* qtd_finais, FILE *fp){
+    // recebe a entrada no formato [estado inicial] [estados finais] [-] [transições];
+    if (fgets(buffer, BUFFER_SIZE, fp) == NULL) return NULL;
     // Salva o estado inicial
     *inicial = (int) buffer[0] - '0';
     // Encontra a quantidade de estados finais
@@ -37,7 +37,7 @@ int** recebe_DFA(char* buffer, int* inicial, int** finais, int* qtd_estados, int
     for (int i = 0; i < (*qtd_estados); i++)
         matriz[i] = (int*) malloc(2 * sizeof(int)); // alfabeto binario
     // Insere os valores na matriz
-    int buffer_pos = (*qtd_finais) + 3; // pula os valores iniciais e o '-1'
+    int buffer_pos = (*qtd_finais) + 2; // pula os valores iniciais e o '-'
     for (int i = 0; i < (*qtd_estados); i++)
         for (int j = 0; j < 2; j++)
             matriz[i][j] = buffer[buffer_pos++] - '0';
@@ -45,8 +45,8 @@ int** recebe_DFA(char* buffer, int* inicial, int** finais, int* qtd_estados, int
     return matriz;
 }
 
-int* recebe_string(char* buffer, int* tamanho_func){
-    if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) return NULL;
+int* recebe_string(char* buffer, int* tamanho_func, FILE *fp){
+    if (fgets(buffer, BUFFER_SIZE, fp) == NULL) return NULL;
     *tamanho_func = strlen(buffer);
     // Elimina caracteres extras
     if (*tamanho_func > 0 && buffer[(*tamanho_func) - 1] == '\n')
@@ -100,30 +100,42 @@ void libera_matriz(int** matrix, int qtd_linhas){
 //    }
 //}
 
-int main(){
-    char buffer[BUFFER_SIZE];
-    int inicial, qtd_finais, qtd_estados, tamanho_func;
-    int *finais, *func_ext;
-    // Instruções para o usuário
-    printf("Escreva o DFA seguindo o seguinte formato: <ESTADO INICIAL><ESTADOS FINAIS>-1<TRANSICOES>\n");
-    printf("EXEMPLO: 134-12103412444\n");
-    printf("DESCRICAO\nestado inicial: 1\nestados finais: 3,4\nmatriz de transicao:\n");
-    printf("   0: 2 | 1\n-> 1: 0 | 3\n   2: 4 | 1\n * 3: 2 | 4\n * 4: 4 | 4\n\n");
+int main(int argc, char *argv[]){
+    if (argc < 2) {
+        // Instruções para o usuário
+        printf("Se voce esta vendo esta mensagem, significa que não executou o programa corretamente.\n");
+        printf("Ao executar pelo terminal, adicione o nome de um arquivo .txt que contenha o automato na primeira linha e as strings nas linhas subseguintes.\n");
+        printf("INSTRUCOES\n");
+        printf("Escreva o DFA seguindo o seguinte formato: <ESTADO INICIAL><ESTADOS FINAIS>-<TRANSICOES>\n");
+        printf("EXEMPLO: 134-2103412444\n");
+        printf("DESCRICAO\nestado inicial: 1\nestados finais: 3,4\nmatriz de transicao:\n");
+        printf("   0: 2 | 1\n-> 1: 0 | 3\n   2: 4 | 1\n * 3: 2 | 4\n * 4: 4 | 4\n\n");
+        return 0;
+    } else {
+        char buffer[BUFFER_SIZE];
+        int inicial, qtd_finais, qtd_estados, tamanho_func;
+        int *finais, *func_ext;
+        int **dfa;
+        FILE *fp;
 
-    // Leitura
-    int **dfa = recebe_DFA(buffer, &inicial, &finais, &qtd_estados, &qtd_finais);
-    //imprime_matriz(dfa, qtd_estados);
-    int index = 1;
-    while (true) {
-        func_ext = recebe_string(buffer, &tamanho_func);
-        if (!func_ext) break;
-        // Validação
-        else valida_funcao(dfa, func_ext, finais, inicial, qtd_finais, tamanho_func, index++);
-        free(func_ext);
+        for (int i = 1; i < argc; i++) {
+            fp = fopen(argv[i], "r");
+            // Leitura
+            dfa = recebe_DFA(buffer, &inicial, &finais, &qtd_estados, &qtd_finais, fp);
+            //imprime_matriz(dfa, qtd_estados);
+            int index = 1;
+            while (true) {
+                func_ext = recebe_string(buffer, &tamanho_func, fp);
+                if (!func_ext) break;
+                // Validação
+                else valida_funcao(dfa, func_ext, finais, inicial, qtd_finais, tamanho_func, index++);
+                free(func_ext);
+            }
+            // Liberação das alocações dinâmicas
+            libera_matriz(dfa, qtd_estados);
+            free(finais);
+        }
+
+        return 0;
     }
-
-    // Liberação das alocações dinâmicas
-    libera_matriz(dfa, qtd_estados);
-    free(finais);
-    return 0;
 }
